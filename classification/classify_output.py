d086174e91dbc435588622c6c7b7a34b38753d46
Em-R2019 <emmyvdree@gmail.com>
2023-11-27 16:54:02 +0100

    output

--- classification/classify_output.py
+++ classification/classify_output.py
@@ -5,6 +5,7 @@ import pandas as pd
 threshold = 0.23
 datalabel = "cns"
 data_path = '../data/output/FULL/CNS/pubmed_abstract_20.11_10.34_epoch15.csv'
+full_model_name = "pubmed_abstract_20.11_10.34_epoch15"
 
 df_output = pd.read_csv(data_path)
 
@@ -28,12 +29,12 @@ df_included = df_output[decision_bool]
 df_included = df_included.loc[:, 'titleabstract']
 
 df_excluded = df_output[~decision_bool]
-df_excluded = df_excluded[df_excluded["prediction"] > 0.25]
+df_excluded = df_excluded[df_excluded["prediction"] > 0.13]
 
 output_folder = "../data/output/FULL"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-df_output.to_csv(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_DECISION2.csv")
-df_included.to_csv(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_INCLUDED2.csv")
-df_excluded.to_excel(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_check2.xlsx")
+df_output.to_csv(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_{full_model_name}_DECISION.csv")
+df_included.to_csv(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_{full_model_name}_INCLUDED.csv")
+df_excluded.to_excel(f"{output_folder}/{datalabel.upper()}/{datalabel.upper()}_{full_model_name}_check.xlsx")
